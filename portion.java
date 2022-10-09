5545a8cdc149c2511d5336e475f941cd8860da37
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-08 22:58:28 -0300

    New problem

---
+++ portion.java
@@ -0,0 +1,26 @@
+package portion;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Integer curupira = 300;
+        Integer boitata = 1500;
+        Integer boto = 600;
+        Integer mapinguari = 1000;
+        Integer iara = 150;
+        Integer i;
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> cassava = new ArrayList<>();
+        for(i = 0; i <= 5; i++){
+            cassava.add(scanner.nextInt());
+        }
+
+    }
+ 
+}
