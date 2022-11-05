1cffd21de2969b3276a183b24007345169ff165e
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-05 20:27:16 -0300

    Added string

--- reading/reading/SeveralReadings.java
+++ reading/reading/SeveralReadings.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class SeveralReadings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String text;
+
+        System.out.println("Enter your name");
+        text = scanner.nextLine();
+
+        System.out.println("Your name is " + text);
+
         
+
     }
 }
