19967393e619efc69dc73e7f20dc35fb786a1309
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-05 19:51:22 -0300

    Starting a new code

---
+++ reading/reading/Reading.java
@@ -0,0 +1,12 @@
+package reading;
+
+import java.util.Scanner;
+
+public class Reading{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close(); //it is good practice to close the scanner at the end of its use in the code
+    }
+}
