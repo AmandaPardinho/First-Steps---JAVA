e8bcb25eeb8601bd6a319e5bf3781be5e638742a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-05 20:03:23 -0300

    Using java.util.Scanner

--- reading/reading/Reading.java
+++ reading/reading/Reading.java
@@ -7,6 +7,17 @@ public class Reading{
         
         Scanner scanner = new Scanner(System.in);
 
+        int integerValue;
+        float floatValue;
+
+        System.out.println("Please enter an integer value");
+        integerValue = scanner.nextInt();
+
+        System.out.println("Please enter a real value");
+        floatValue = scanner.nextFloat();
+
+        System.out.println("You entered the value " + integerValue + " and " + floatValue);
+
         scanner.close(); //it is good practice to close the scanner at the end of its use in the code
     }
 }
