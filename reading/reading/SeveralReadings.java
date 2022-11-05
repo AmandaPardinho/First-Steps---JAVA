8152f07429fa3cc495e62613a5bb3c72fd11fcca
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-05 20:21:25 -0300

    Starting a new java class

---
+++ reading/reading/SeveralReadings.java
@@ -0,0 +1,10 @@
+package reading;
+
+import java.util.Scanner;
+
+public class SeveralReadings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
