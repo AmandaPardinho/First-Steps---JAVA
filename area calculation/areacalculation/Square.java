327811010440c34f2dd9c5642994d626b43825ad
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-06 23:01:29 -0300

    Area of the square

---
+++ area calculation/areacalculation/Square.java
@@ -0,0 +1,22 @@
+package areacalculation;
+
+import java.util.Scanner;
+
+public class Square{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int areaSquare;
+        int side = 0;
+
+        System.out.println("Enter the value of the side of the square (meters):");
+        side = scanner.nextInt();
+
+        areaSquare = side * side;
+
+        System.out.println("The area of the square is " + areaSquare + " m².");
+
+        scanner.close();
+    }
+}
